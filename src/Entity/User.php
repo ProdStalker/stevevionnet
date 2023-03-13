ec1ecc19c3ae8576068c7f0d8fbfe2abe410f60a
Prod Stalker <prod.stalker@gmail.com>
2023-03-13 13:42:03 +0100

    Add getFullName method

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -141,4 +141,10 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function getFullName(): ?string
+    {
+        return sprintf('%s %s', $this->getLastName(), $this->getFirstName());
+    }
+
 }
