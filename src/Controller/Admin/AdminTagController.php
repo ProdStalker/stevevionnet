db9e566dfc9c6e4f03941f0c09ad5c7a1e1949ae
Prod Stalker <prod.stalker@gmail.com>
2023-04-05 10:20:48 +0200

    Add, edit and new tag

---
+++ src/Controller/Admin/AdminTagController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Tag;
+use App\Form\TagType;
+use App\Repository\TagRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin/tag', name: 'admin_tag_')]
+class AdminTagController extends AbstractController
+{
+    public function __construct() {
+
+    }
+
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Tag $tag, TagRepository $tagRepository): Response
+    {
+        $form = $this->createForm(TagType::class, $tag);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $tagRepository->save($tag, true);
+
+            return $this->redirectToRoute('admin_tag_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin/tag/edit.html.twig', [
+            'tag' => $tag,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/', name: 'index', methods: ['GET'])]
+    public function index(TagRepository $tagRepository): Response
+    {
+        return $this->render('admin/tag/index.html.twig', [
+            'tags' => $tagRepository->findAll()
+        ]);
+    }
+
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
+    public function new(Request $request, TagRepository $tagRepository): Response
+    {
+        $tag = new Tag();
+        $form = $this->createForm(TagType::class, $tag);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $tagRepository->save($tag, true);
+
+            return $this->redirectToRoute('admin_tag_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('admin/tag/new.html.twig', [
+            'tag' => $tag,
+            'form' => $form,
+        ]);
+    }
+}
