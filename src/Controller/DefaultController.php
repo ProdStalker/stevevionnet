c1d97e7ec9d47c2c2a9b916b7d059d6826f53f0d
ProdStalker <prod.stalker@gmail.com>
2024-01-08 09:59:29 +0100

    Choose category homepage in code

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -12,6 +12,7 @@ class DefaultController extends AbstractController
     public function index(): Response
     {
         return $this->render('default/index.html.twig', [
+            'categoryName' => 'Programming'
         ]);
     }
 }
