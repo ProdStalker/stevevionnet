bc374a97f2a84b7e9af8ef4816898a5404750b6c
ProdStalker <prod.stalker@gmail.com>
2024-01-08 11:56:23 +0100

    retrieve project homepage

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Project;
+use App\Utils\ProjectUtil;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(ProjectUtil $projectUtil): Response
     {
+        /**
+         * @var Project[] $projects
+         */
+        $projects = $projectUtil->homepageProjects();
+
         return $this->render('default/index.html.twig', [
-            'categoryName' => 'Programming'
+            'categoryName' => 'Programming',
+            'projects' => $projects
         ]);
     }
 }
