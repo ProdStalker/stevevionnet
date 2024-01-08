9c60957b12e93218bd33aef4af09af8eeaf4dffc
ProdStalker <prod.stalker@gmail.com>
2024-01-08 11:55:54 +0100

    method to get homepage projects

---
+++ src/Utils/ProjectUtil.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Utils;
+
+use App\Entity\Project;
+use App\Repository\ProjectRepository;
+use Psr\Cache\CacheItemInterface;
+
+class ProjectUtil extends EntityUtil
+{
+    protected function getRepository(): ProjectRepository
+    {
+        return $this->em->getRepository(Project::class);
+    }
+
+    public function homepageProjects(): array
+    {
+        return $this->cache->get('homepage-projects', function (CacheItemInterface $item) {
+            $item->expiresAfter(30);
+            return $this->getRepository()->findAll();
+        });
+    }
+}
